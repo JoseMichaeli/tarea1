c03b683a6edb7763fb0052ce040553edcc12a29f
JoseMichaeli <josemichaeli@gmail.com>
2021-04-11 22:56:40 -0300

    catch error

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -3,23 +3,29 @@ import data from './../../data'
 import { Link } from "react-router-dom";
 
 const Details = ({ }) => {
-    const id = window.location.pathname.split("/")[2];
-    //const contacto = data.filter(contact => { if(contact.id === parseInt(id,10)){ return contact;}} );
-    const contacto = data.filter(contact => { return contact.id === parseInt(id,10); });
-    const {first_name, last_name, number } = contacto[0];
+    try {
+        const id = window.location.pathname.split("/")[2];
+        //const contacto = data.filter(contact => { if(contact.id === parseInt(id,10)){ return contact;}} );
+        const contacto = data.filter(contact => { return contact.id === parseInt(id, 10); });
+        const { first_name, last_name, number } = contacto[0];
+        return (
+            <>
+                <h4>Nombre: {first_name}</h4>
+                <h4>Apellido: {last_name}</h4>
+                <h4>Telefono: {number}</h4>
+                <Link to={'/'}>Back</Link>
 
-    
-    //const name = first_name;
-    return (
-        <>
-            <h4>Nombre: {first_name}</h4>
-            <h4>Apellido: {last_name}</h4>
-            <h4>Telefono: {number}</h4>
-            <Link to={'/'}>Back</Link>
-          
-                
 
-        </>
-    )
+
+            </>
+        )
+    } catch (error) {
+        return (
+            <>
+                <h2> Page not Found.</h2>
+                <Link to={'/'}>Back</Link>
+            </>
+        );
+    }
 };
 export default Details;
